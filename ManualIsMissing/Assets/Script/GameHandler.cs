616dafbac122387c52baf47551c638fdd88f2d3c
Jean Tan Li <chines1998@hotmail.com>
2019-08-11 22:38:33 -0300

    Inicio do projeto

---
+++ ManualIsMissing/Assets/Script/GameHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("GameStart");
+
+        GameObject gameObject = new GameObject("Pipe", typeof(SpriteRenderer));
+    }
+
+}
